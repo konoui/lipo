bf6a603ab0b19606cb41530a997ae78d88fa1281
konoui <konoui.dev@gmail.com>
2023-07-04 22:13:40 +0900

    update usage

--- cmd/const.go
+++ cmd/const.go
@@ -2,7 +2,7 @@ package cmd
 
 const (
 	createDescription = `
-Create an universal binary(fat binary) from input thin binaries. 
+Create a universal binary (also known as a fat binary) from input thin binaries.
 e.g. lipo path/to/binary.x86_64 path/to/binary.arm64e -create -output path/to/fat-binary
 `
 	extractDescription = `
@@ -19,28 +19,28 @@ e.g. lipo path/to/fat-binary -remove x86_64 -remove x86_64h -output path/to/new-
 `
 	replaceDescription = `
 Replace the specified architecture in a universal binary with the specified input binary.
-e.g. lipo path/to/fat-binary -replace x86_64 path/to/binary.x86_64 -output path/to/new-fat-binary.
+e.g. lipo path/to/fat-binary -replace x86_64 path/to/binary.x86_64 -output path/to/new-fat-binary
 `
 	thinDescription = `
-Extract a single-architecture-binary from a universal binary and create a single binary.
+Extract a single-architecture binary from a universal binary and create a single binary.
 e.g. lipo path/to/fat-binary -thin arm64e -output path/to/binary.arm64e
 `
 	archsDescription = `
-List all architectures contained in a universal binary
+List all architectures contained in a universal binary.
 e.g. lipo path/to/fat-binary -archs
 `
 	verifyArchDescription = `
-Verify the specified architecture are present in an universal binary.
-If present, exit status of 0 otherwise exit status of 1.
-e.g. lipo path/to/fat-binary x86_64 x86_64h arm64 arm64e
+Verify that the specified architectures are present in a universal binary.
+If present, the exit status is 0; otherwise, the exit status is 1.
+e.g. lipo path/to/fat-binary x86_64 x86_64h arm64 arm64e -verify_arch
 `
 	infoDescription = `
-List architectures in the input universal binary.
+Display brief information on architectures in universal binaries.
 e.g. lipo path/to/fat-binary path/to/binary.x86_64 -info
 `
 
 	detailedInfoDescription = `
 Display detailed information about universal binaries.
-e.g. lipo path/to/fat-binary -detailed_info
+e.g. lipo path/to/fat-binary path/to/binary.x86_64 -detailed_info
 `
 )
